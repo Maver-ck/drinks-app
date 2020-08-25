d009e6e5a270e3607dd764bdde29a219355a93cc
Yasser Ansari <yasser.ansari@jhc.co.uk>
2020-08-25 13:44:22 +0100

    Updates

---
+++ precache-manifest.ecf2c694f3ba9188b96620be7fe91eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fcff4fd024431062342613dde5726d1",
+    "url": "/drinks-app/index.html"
+  },
+  {
+    "revision": "5c183523c48f0f9c1067",
+    "url": "/drinks-app/static/css/main.037adc13.chunk.css"
+  },
+  {
+    "revision": "768369218f7f3c791acb",
+    "url": "/drinks-app/static/js/2.651258fd.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/drinks-app/static/js/2.651258fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c183523c48f0f9c1067",
+    "url": "/drinks-app/static/js/main.6b577b24.chunk.js"
+  },
+  {
+    "revision": "933d08c328453ab960f5",
+    "url": "/drinks-app/static/js/runtime-main.61ef6c7c.js"
+  }
+]);
